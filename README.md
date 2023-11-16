2ac3082583a121dc9da0ca84c0fff91219b1e590
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-11-16 15:55:59 +0900

    🧪 Check side-effect of `Dollar`

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 - [ ] $5 + 10CHF = $10 (assuming the exchange rate is 2:1)
 - [x] $5 \* 2 = $10
 - [ ] Make `amount` private
-- [ ] Dollar side effects?
+- [ ] **Dollar side effects?**
 - [ ] Money rounding?
--- src/test/java/tdd/DollarTest.java
+++ src/test/java/tdd/DollarTest.java
@@ -11,5 +11,7 @@ class DollarTest {
         Dollar five = new Dollar(5);
         five.times(2);
         assertEquals(10, five.amount);
+        five.times(3);
+        assertEquals(15, five.amount);
     }
 }
