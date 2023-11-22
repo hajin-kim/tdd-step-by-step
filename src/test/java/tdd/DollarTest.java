7da0121e36c0134fe46d87e502c9c73858803c7c
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-11-22 14:52:17 +0900

    ✅ Simplify test code by inlining variable

--- src/test/java/tdd/DollarTest.java
+++ src/test/java/tdd/DollarTest.java
@@ -10,10 +10,8 @@ class DollarTest {
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(new Dollar(10), product);
-        product = five.times(3);
-        assertEquals(new Dollar(15), product);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
