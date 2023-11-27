4d46e936dccde4e6958ecbad36a1c00699d983f5
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-11-27 14:58:11 +0900

    ✅ Copy `Dollar` into `Franc`

--- README.md
+++ README.md
@@ -11,4 +11,7 @@
 - [ ] `hashCode()`
 - [ ] Equal null
 - [ ] Equal object
-- [ ] **5CHF \* 2 = 10CHF**
+- [x] 5CHF \* 2 = 10CHF
+- [ ] `Dollar`/`Franc` duplication
+- [ ] Common `equals()`
+- [ ] Common `times()`
---
+++ src/main/java/tdd/Franc.java
@@ -0,0 +1,19 @@
+package tdd;
+
+public class Franc {
+    private int amount;
+
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    public Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        Franc dollar = (Franc) object;
+        return amount == dollar.amount;
+    }
+}
