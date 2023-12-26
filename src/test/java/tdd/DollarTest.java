6d4ba070d6371583442573adf08b2dac7daacc73
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-12-27 01:46:28 +0900

    🧪 Reduce reference to subclass

--- src/test/java/tdd/DollarTest.java
+++ src/test/java/tdd/DollarTest.java
@@ -9,7 +9,7 @@ class DollarTest {
 
     @Test
     void testMultiplication() {
-        Dollar five = Money.dollar(5);
+        Money five = Money.dollar(5);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
