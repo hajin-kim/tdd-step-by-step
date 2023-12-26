ad3cab7b98dfb6c9fec44a89974b696477ddad39
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-12-27 01:50:26 +0900

    :recycle: Apply factory method to test code

--- src/test/java/tdd/DollarTest.java
+++ src/test/java/tdd/DollarTest.java
@@ -3,14 +3,13 @@ package tdd;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class DollarTest {
 
     @Test
     void testMultiplication() {
         Money five = Money.dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 }
--- src/test/java/tdd/MoneyTest.java
+++ src/test/java/tdd/MoneyTest.java
@@ -8,14 +8,14 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 class MoneyTest {
     @Test
     void testEquality() {
-        assertEquals(new Dollar(5), new Dollar(5));
-        assertNotEquals(new Dollar(5), new Dollar(6));
+        assertEquals(Money.dollar(5), Money.dollar(5));
+        assertNotEquals(Money.dollar(5), Money.dollar(6));
         assertEquals(new Franc(5), new Franc(5));
         assertNotEquals(new Franc(5), new Franc(6));
     }
 
     @Test
     void testDifferentClassEquality() {
-        assertNotEquals(new Dollar(5), new Franc(5));
+        assertNotEquals(Money.dollar(5), new Franc(5));
     }
 }
