b1872f7adab6fb7a57e5bb2e579f449f966e84b7
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-12-27 01:44:25 +0900

    🧪 Assert factory method for `Dollar` instantiation

--- src/test/java/tdd/DollarTest.java
+++ src/test/java/tdd/DollarTest.java
@@ -9,7 +9,7 @@ class DollarTest {
 
     @Test
     void testMultiplication() {
-        Dollar five = new Dollar(5);
+        Dollar five = Money.dollar(5);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
